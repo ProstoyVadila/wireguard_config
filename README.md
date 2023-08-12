0a9f4eee0840c638263a9792451899fbe10652aa
Vadim Gorbachev <vadim.gorbachev.dev@gmail.com>
2023-08-12 23:06:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# Wireguard VPN Configuration
+
+## TODO
+
+1. Get a VPS instance somewhere.
+2. Check a port for your vpn server. It should be open.
+3. [Install `wireguard`.](#install-wireguard)
+4. [Generate private and public keys for your server.](#keys-generation)
+5. [Check your network configuration on your VPS (more likely `eth0`).](#check-network-configuration)
+6. Create configuration file (use [`wg0.conf`](wg0.conf) as example).
+7. [Set ip fastforwarding.](#set-fastforwarding)
+8. Set your server configuration to the [script](add_user.sh)
+9. [Add users](#add-users-script) running the [script](add_user.sh).
+10. [Start system demon.](#system-deamon)
+11. Download app to your client device.
+12. Open your client config on your device.
+13. Enjoy your little fictional piece of freedom. ヾ(⌐■_■)ノ ♪
+
+### Install Wireguard
+
+```bash
+apt update && apt -y upgrade
+```
+
+```bash
+apt install -y wireguard
+```
+
+### Keys Generation
+
+Don't copy! Use as example.
+
+```bash
+wg genkey | tee /etc/wireguard/<server_private_key_name> | wg pubkey | tee /etc/wireguard/<server_public_key_name>
+```
+
+Don't forget to set rights to your private key.
+
+```bash
+chmod 600 /etc/wireguard/<servier_private_key_name>
+```
+
+### Check Network Configuration
+
+```bash
+ip a
+```
+
+or
+
+```bash
+ifconfig
+```
+
+It is more likely `eth0`. It is used in example [config](wg0.conf). Just set yours.
+
+### Set fastforwarding
+
+```bash
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+```
+
+### System deamon
+
+Enable
+
+```bash
+systemctl enable wg-quick@wg0.service
+```
+
+\
+Start
+
+```bash
+systemctl start wg-quick@wg0
+```
+
+\
+Stop
+
+```bash
+systemctl stop wg-quick@wg0
+```
+
+\
+Restart:
+
+```bash
+systemctl restart wg-quick@wg0
+```
+
+\
+Check status:
+
+```bash
+systemctl status wg-quick@wg0
+```
+
+\
+Check wireguard stats
+
+```bash
+wg show
+```
+
+### Add Users Script
+
+Give it executable rights for the first time.
+
+```bash
+chmod +x add_user.sh
+```
+
+To add user:
+
+```bash
+./add_user.sh <user_name>
+```
---
+++ add_user.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# TODO: set your server configuration info here
+SERVER_PUBLIC_KEY_NAME=
+SERVER_IP=
+DNS="8.8.8.8"  # example
+CONFIG_TAG=vpn # example
+
+# Consts
+CLIENT_PRIVATE_KEY_NAME="$1"_privatekey # example
+CLIENT_PUBLIC_KEY_NAME="$1"_publickey   # example
+ALLOWED_IPS="0.0.0.0/0"                 # example
+PERSISTENT_KEEP_ALIVE="20"              # example
+
+CLIENT_CONFIG_PATH="./"
+CLIENT_CONFIG_FILE="$1"_"$CONFIG_TAG".conf
+CLIENT_CONFIG_FILEPATH="$CLIENT_CONFIG_PATH/$CLIENT_CONFIG_FILE"
+
+CONFIG_PATH=/etc/wireguard
+CONFIG_FILE=wg0.conf
+CONFIG_FILEPATH="$CONFIG_PATH/$CONFIG_FILE"
+
+BACKUP_FOLDER=/etc/wireguard/backup
+
+set -e
+
+# Check new client config name
+if [ -z "$1" ]; then
+    echo "Enter a username for the new vpn config"
+    exit 1
+fi
+
+# Check if printf exists in that bash
+if [ ! "$(command -v printf)" ]; then
+    echo "command \"printf\" does not exists on system."
+    echo "please, install \"printf\""
+    exit 1
+fi
+
+# Checking installed wireguard
+if [ ! -d "$CONFIG_PATH" ]; then
+    printf "Cannot find a wireguard folder %s" "$CONFIG_PATH"
+    exit 1
+fi
+
+# Createing/updating a folder of previous verions of the server config file
+if [ ! -d "$BACKUP_FOLDER" ]; then
+    mkdir $BACKUP_FOLDER
+fi
+
+# Checking config existance
+if [[ ! -f $CONFIG_FILEPATH ]]; then
+    echo "Cannot find the wireguard server config file $CONFIG_FILEPATH"
+    exit 1
+fi
+
+# Copying the server config to backup folder
+BACKUP_SERVER_CONFIG_FILEPATH="$BACKUP_FOLDER/$CONFIG_FILE-$(date +%Y%m%d)"
+cp $CONFIG_FILEPATH "$BACKUP_SERVER_CONFIG_FILEPATH"
+
+# Checking server public key existance
+if [[ ! -f "$CONFIG_PATH/$SERVER_PUBLIC_KEY_NAME" ]]; then
+    echo "Cannot find the server public key"
+    exit 1
+fi
+SERVER_PUBLIC_KEY=$(cat "$CONFIG_PATH/$SERVER_PUBLIC_KEY_NAME")
+
+# Getting the port of vpn server
+SERVER_PORT=$(grep "ListenPort" $CONFIG_FILEPATH | awk '{print $3}')
+if [ -z "$SERVER_PORT" ]; then
+    echo "Cannot find a server port :("
+    exit 1
+fi
+ENDPOINT="$SERVER_IP/$SERVER_PORT"
+
+# Getting the last part of the last client IP
+IP_ID=$(grep "10.0.0." $CONFIG_FILEPATH -c)
+IP_ID=$((IP_ID + 1))
+if [ -z "$IP_ID" ]; then
+    echo "Cannot find a last client ip address"
+    exit 1
+fi
+CLIENT_ADDRESS="10.0.0.$IP_ID/32"
+
+# Generating the new client config
+echo "Creating a pair of keys for the new client config..."
+wg genkey | tee "$CONFIG_PATH/$CLIENT_PRIVATE_KEY_NAME" | wg pubkey | tee "$CONFIG_PATH/$CLIENT_PUBLIC_KEY_NAME"
+
+CLIENT_PRIVATE_KEY=$(cat "$CONFIG_PATH/$CLIENT_PRIVATE_KEY_NAME")
+CLIENT_PUBLIC_KEY=$(cat "$CONFIG_PATH/$CLIENT_PUBLIC_KEY_NAME")
+
+# Adding the new client to the server config
+echo "Adding the new client to the server config..."
+{
+    printf "\n[Peer]"
+    printf "\nPublicKey = %s" "$CLIENT_PUBLIC_KEY"
+    printf "\nAllowIPs = %s" "$CLIENT_ADDRESS"
+
+} >>$CONFIG_FILEPATH
+
+# Creating the new client config
+echo "Creating a client config file..."
+if [ -n "$CLIENT_CONFIG_PATH" ]; then
+    mkdir -p $CLIENT_CONFIG_PATH
+fi
+touch "$CLIENT_CONFIG_FILEPATH"
+
+{
+    printf "[Interface]"
+    printf "\nPrivateKey = %s" "$CLIENT_PRIVATE_KEY"
+    printf "\nAddress = %s" "$CLIENT_ADDRESS"
+    printf "\nDNS = %s" "$DNS"
+    printf "\n"
+    printf "\n[Peer]"
+    printf "\nPublicKey = %s" "$SERVER_PUBLIC_KEY"
+    printf "\nEndpoint = %s" "$ENDPOINT"
+    printf "\nAllowedIPs = %s" "$ALLOWED_IPS"
+    printf "\nPersistentKeepalive = %s" "$PERSISTENT_KEEP_ALIVE"
+
+} >>"$CLIENT_CONFIG_FILEPATH"
+
+# Restarting server
+systemctl restart wg-quick@wg0
+systemctl status wg-quick@wg0
+wg show
+
+printf "Successfully added user %s to vpn!" "$1"
+printf "The new client configuration file is here %s" "$CLIENT_CONFIG_FILEPATH"
